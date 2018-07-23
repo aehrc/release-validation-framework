df0a586d288afc40c72b72c7ca75beb49a7f9ecd
Huy Le <huyhu@netcompany.com>
2018-07-23 09:35:29 +0700

    Added missing assertion for US, re-order sections in report

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-active-inactive-term-match-authoring.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-description-active-inactive-term-match-authoring.sql
@@ -21,8 +21,8 @@
 /* 	limit to a list of active concepts of which descriptions have been edited 
 	this release 
 */
-	drop table if exists tmp_edited_con;
-	create table if not exists tmp_edited_con as
+	drop table if exists tmp_edited_con_auth;
+	create table if not exists tmp_edited_con_auth as
 	select distinct a.*
 	from curr_concept_s a
 		join curr_description_d b
@@ -30,28 +30,28 @@
 			and a.active = 1;
 
 /* list of active description of active concepts edited for this release */
-	drop table if exists tmp_active_desc;
-	create table if not exists tmp_active_desc as
+	drop table if exists tmp_active_desc_auth;
+	create table if not exists tmp_active_desc_auth as
 	select a.*
 	from curr_description_d a 
 	where a.active=1 and not exists (select count(*) as total from prev_description_s b where a.id=b.id having total >= 1);
 	
-	alter table tmp_active_desc add index idx_tmp_ad_cid(conceptid);
-	alter table tmp_active_desc add index idx_tmp_ad_a(active);
-	alter table tmp_active_desc add index idx_tmp_ad_t(term);
+	alter table tmp_active_desc_auth add index idx_tmp_ad_cid(conceptid);
+	alter table tmp_active_desc_auth add index idx_tmp_ad_a(active);
+	alter table tmp_active_desc_auth add index idx_tmp_ad_t(term);
 	
 /* list of inactive description of active concepts edited for this release */
-	drop table if exists tmp_inactive_desc;
-	create table if not exists tmp_inactive_desc as
+	drop table if exists tmp_inactive_desc_auth;
+	create table if not exists tmp_inactive_desc_auth as
 	select a.*
 	from curr_description_s a
-		join tmp_edited_con b
+		join tmp_edited_con_auth b
 			on a.conceptid = b.id
 			and a.active = 0;
 	
-	alter table tmp_inactive_desc add index idx_tmp_id_cid(conceptid);
-	alter table tmp_inactive_desc add index idx_tmp_id_a(active);
-	alter table tmp_inactive_desc add index idx_tmp_id_t(term);
+	alter table tmp_inactive_desc_auth add index idx_tmp_id_cid(conceptid);
+	alter table tmp_inactive_desc_auth add index idx_tmp_id_a(active);
+	alter table tmp_inactive_desc_auth add index idx_tmp_id_t(term);
 
 /* 	violators are active descriptions of which the terms are the same as 
 	inactive descriptions for a given concept 
@@ -62,14 +62,14 @@
 		'<ASSERTIONUUID>',
 		a.conceptid,
 		concat('Active description id = ',a.id,' and inactive description id = ', b.id, ' share the same term') 
-	from tmp_active_desc a
-	join tmp_inactive_desc b
+	from tmp_active_desc_auth a
+	join tmp_inactive_desc_auth b
 	on a.conceptid = b.conceptid
 	and a.moduleid = b.moduleid
 	and cast(a.term as binary) = cast(b.term as binary)
 	where a.active != b.active
 	and cast(a.effectivetime as datetime) >= cast(b.effectivetime as datetime);
 
-	drop table if exists tmp_edited_con;
-	drop table if exists tmp_active_desc;
-	drop table if exists tmp_inactive_desc;
+	drop table if exists tmp_edited_con_auth;
+	drop table if exists tmp_active_desc_auth;
+	drop table if exists tmp_inactive_desc_auth;
