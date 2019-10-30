c46edbff3110863d46cdde4e42fdc3491e659ad8
Huy Le <huy.le@netcompany.com>
2019-10-30 14:32:05 +0700

    RQI-60: Refactored assertion for map refsets to check for valid referenced concept IDs

--- importer/src/main/resources/scripts/component-centric/component-centric-snapshot-extended-maps-referencedcomponentid-is-active-concept.sql
+++ importer/src/main/resources/scripts/component-centric/component-centric-snapshot-extended-maps-referencedcomponentid-is-active-concept.sql
@@ -2,23 +2,9 @@
 	component-centric-snapshot-extended-maps-referencedcomponentid-is-active-concept
 
 	Assertion:
-	Extended Map Refset Snapshot files has valid, active SNOMED CT concepts in the ReferencedComponentId
+	Extended Map Refset Snapshot files has existing SNOMED CT concepts in the ReferencedComponentId
 
 ********************************************************************************/
-   insert into qa_result (runid, assertionuuid, concept_id, details)
-	select
-		<RUNID>,
-		'<ASSERTIONUUID>',
-		a.moduleid,
-		concat('EXTENDED MAP REFSET SNAPSHOT: id=',a.id,' has inactive concept without at lease 1 active descriptions in ReferencedComponentId = ', a.referencedcomponentid)
-        from curr_extendedmaprefset_s a
-        where a.referencedcomponentid not in (
-            select distinct conceptId
-            from curr_description_s b
-            where b.active = 1
-            group by conceptId
-        );
-    commit;
 
     insert into qa_result (runid, assertionuuid, concept_id, details)
     select
